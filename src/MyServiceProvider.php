3d52cf453c3509393abe22e7ee4ba264820e20e0
MD. SAZZAD <mdsazzad0002@gmail.com>
2024-09-05 21:57:39 +0600

    Initial commit

---
+++ src/MyServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+namespace Vendor\bkash;
+
+use Illuminate\Support\ServiceProvider;
+
+class MyServiceProvider extends ServiceProvider{
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Register package services here
+    }
+
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Bootstrap any package services here
+    }
+}
